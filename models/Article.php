1e4567d2fa17420e6a6bee61a3ee51a437e279ab
Jiri Knesl <jiri.knesl@gmail.com>
2010-08-11 07:20:12 +0200

    Destroy sqlite & take care of model

--- models/Article.php
+++ models/Article.php
@@ -1,6 +1,18 @@
 <?php
 class Article {
 	static function findBests() {
-		return dibi::fetchAll("SELECT * FROM [article] WHERE [best]");
+		try {
+			return dibi::fetchAll("SELECT * FROM [article] WHERE [best]");
+		} catch (DibiDriverException $e) {
+			// tady dalsi zjistovani, zda se jedna o kritickou chybu
+			// switch ($e->getCode())
+			if (11 == $e->getCode()) {
+				// tabulka je poskozena (zde se jedna o poskozeny soubor, melo by to normalne shodit cely web, ale neudelam tak)
+				return array();
+			} else {
+				throw $e;
+			}
+		} 
+		// samotnou DibiException necham vybublat
 	}
 }
